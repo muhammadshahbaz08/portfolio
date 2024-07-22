4df385e102d0dc82b8910463decf804594c1685e
Muhammad Shahbaz <38607213+muhammadshahbaz08@users.noreply.github.com>
2024-07-22 18:59:53 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Hero from "./component/hero";
 import About from "./component/about";
+import Projects from "./component/projects";
+import Contact from "./component/contact";
 export default function Home() {
   return (
     <main>
       <Hero />
       <About />
+      <Projects />
+      <Contact />
     </main>
   );
 }
